b68139072907712b51d30a92e2699c0f15ee6a25
VMovsisyan <vmovsisyan@users.noreply.github.com>
2014-09-30 15:02:35 +0400

    Add node app

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+var app = require('../app.js');
+ 
+describe('GET /', function() {
+  it('respond with hello world', function(done) {
+    request(app).get('/').expect('hello world', done);
+  });
+});
+
